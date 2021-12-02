f05460962f825746d563bc569ddfba309e4618bc
金剑辉 <42485117+jinjianhui@users.noreply.github.com>
2021-12-02 23:19:02 +0800

    列表

--- 07.EMS练习.py
+++ 07.EMS练习.py
@@ -1,100 +1,94 @@
-print("="*20,"欢迎使用员工管理系统","="*20)
-# 创建一个列表
-staff_messages = ['孙悟空\t500\t男\t花果山']
+# 显示系统的欢迎信息
+print('-'*20 , '欢迎使用员工管理系统', '-'*20)
+# 创建一个列表，用来保存员工的信息，员工的信息以字符串的形式统一保存到列表
+emps = ['孙悟空\t18\t男\t花果山','猪八戒\t28\t男\t高老庄']
+
 # 创建一个死循环
 while True:
-    # 方法1
-    # control = ['1、查询员工','2、添加员工','3、删除员工','4、退出系统']
-    # for i in control:
-    #     print(i)
-    # print("请选择要做的操作：")
-    # 方法2
-    print("\t1、查询员工")
-    print("\t2、添加员工")
-    print("\t3、删除员工")
-    print("\t4、退出系统")
-    user_choose = int(input("请选择（1-4）："))
-    # 打印一个分割线
-    print("="*62)
-
-    if user_choose == 1 :
+    # 显示用户的选项
+    print('请选择要做的操作：')
+    print('\t1.查询员工')
+    print('\t2.添加员工')
+    print('\t3.删除员工')
+    print('\t4.退出系统')
+    user_choose = input('请选择[1-4]:')
+    print('-'*62)
+    # 根据用户的选择做相关的操作
+    if user_choose == '1' :
         # 查询员工
-        # 先打印表头
-        print('序号\t姓名\t年龄\t性别\t住址')
-        # 创建一个变量来显示员工的序号
-        staff_number = 1
+        # 打印表头
+        print('\t序号\t姓名\t年龄\t性别\t住址')
+        # 创建一个变量，来表示员工的序号
+        n = 1
         # 显示员工信息
-        for staff in staff_messages:
-            print(f'{staff_number}\t{staff}')
-            staff_number += 1
-            
-    elif user_choose == 2 :
+        for emp in emps :
+            print(f'\t{n}\t{emp}')
+            n += 1
+    elif user_choose == '2':
         # 添加员工
-        # 获取添加的信息：姓名、年龄、性别、住址
-        name = input('姓名：')
-        age = int(input('年龄：'))
-        gender = input('性别：')
-        addr = input('住址：')
+        # 获取要添加员工的信息，姓名、年龄、性别、住址
+        emp_name = input('请输入员工的姓名：')
+        emp_age = input('请输入员工的年龄：')
+        emp_gender = input('请输入员工的性别：')
+        emp_address = input('请输入员工的住址：')
+
         # 创建员工信息
-        staff = f'{name}\t{age}\t{gender}\t{addr}'
-        # staff_messages.append(name)
-        # staff_messages.append(age)
-        # staff_messages.append(gender)
-        # staff_messages.append(addr)
-        # 提示添加员工信息
-        print('以下员工将会被添加到系统中')
-        print("="*62)
+        # 将四个信息拼接为一个字符串，然后插入到列表中
+        emp = f'{emp_name}\t{emp_age}\t{emp_gender}\t{emp_address}'
+        # 显示一个提示信息
+        print('以下员工将被添加到系统中')
+        print('-'*62)
         print('姓名\t年龄\t性别\t住址')
-        print(staff)
-        print("="*62)
-        judge = input('请确认是否进行该操作：Y/N：')
-        if judge == 'Y':
-            staff_messages.append(staff)
-            print('你已经成功添加该员工至系统中')
-        elif judge == 'N':
-            print('你取消了该员工信息的添加')
-        else:
-            print('你输入的操作有误，请重新选择')
-            break
+        print(emp)
+        print('-'*62)
+        user_confirm = input('是否确认该操作[Y/N]:')
 
-    elif user_choose == 3 :
-        # 删除员工
-        # 删除员工需要通过姓名来删除
-        del_number = int(input('请输入你要删除的员工序号：'))
-        # 判断删除的员工序号
-        if del_number <= len(staff_messages):
-            # 输入合法，根据序号来获取索引：
-            del_i = del_number - 1
-            print('以下员工将会被删除')
-            print("="*62)
-            print('序号\t姓名\t年龄\t性别\t住址')
-            print(f'{del_number}\t{staff_messages[del_i]}')
-            print(staff)
-            print("="*62)
-            judge_confirm = input('该操作不可恢复，请确认是否进行该操作：Y/N：')
-            if judge_confirm == 'Y':
-                staff_messages.remove(del_i)
-                print('你已经成功删除该员工')
-            elif judge_confirm == 'N':
-                print('你取消了该操作！')
-            else:
-                print('你输入的操作有误，请重新选择')
-                break
-        else:
+        # 判断
+        if user_confirm == 'y' or user_confirm == 'yes' :
+            # 确认
+            emps.append(emp)
+            # 显示提示信息
+            print('添加成功！')
+        else :
+            # 取消操作
+            print('添加已取消！')
+        
+    elif user_choose == '3':
+        # 删除员工，根据员工的序号来删除员工
+        # 获取要删除的员工的序号
+        del_num = int(input('请输入要删除的员工的序号：'))
+
+        # 判断序号是否有效
+        if 0 < del_num <= len(emps) :
+            # 输入合法，根据序号来获取索引
+            del_i = del_num - 1
+            # 显示一个提示信息
+            print('以下员工将被删除')
+            print('-'*62)
+            print('\t序号\t姓名\t年龄\t性别\t住址')
+            print(f'\t{del_num}\t{emps[del_i]}')
+            print('-'*62)
+            user_confirm = input('该操作不可恢复，是否确认[Y/N]:')
+            # 判断
+            if user_confirm == 'y' or user_confirm == 'yes' :
+                # 删除元素
+                emps.pop(del_i)
+                # 显示提示
+                print('员工已被删除！')
+            else :
+                # 操作取消
+                print('操作已取消！')
+        else :
+            # 输入有误
             print('您的输入有误，请重新操作！')
-        # del_number = del_user - 1
-        # for user_number in staff_messages:
-        #     del staff_messages[del_number:del_number+4]
-        #     break
-        # del_number += 4
 
-    elif user_choose == 4 :
-        input('感谢使用！再见！点击回车键退出！')
+    elif user_choose == '4':
+        # 退出
+        print('欢迎使用！再见!')
+        input('点击回车键退出！')
         break
-
     else :
         print('您的输入有误，请重新选择！')
-    print("="*62)
 
-
-
+    # 打印分割线
+    print('-'*62)
--- 09.元组.py
+++ 09.元组.py
@@ -1,41 +1,57 @@
 # 元组 tuple
-# 不可变的序列
-# 操作方式基本上和列表时一直的
-# 使用元组时，把元组当做一个不可变的列表
-# 当我们希望数据不改变时，就使用元组
+# 元组是一个不可变的序列
+# 它的操作的方式基本上和列表是一致的
+# 所以你在操作元组时，就把元组当成是一个不可变的列表就ok了
+# 一般当我们希望数据不改变时，就使用元组，其余情况都使用列表
 
 # 创建元组
 # 使用()来创建元组
-my_tuple = ()
-print(type(my_tuple))
+my_tuple = () # 创建了一个空元组
+# print(my_tuple,type(my_tuple)) # <class 'tuple'>
 
-my_tuple = (1,2,3,4,5)
-print(my_tuple[3])
+my_tuple = (1,2,3,4,5) # 创建了一个5个元素的元组
+# 元组是不可变对象，不能尝试为元组中的元素重新赋值
+# my_tuple[3] = 10 TypeError: 'tuple' object does not support item assignment
+# print(my_tuple[3])
 
-# 当元组不是空元组是，括号可以省略
-# 如果元组不是空元组，它里面至少有一个,
-my_tuple = 1,
-my_tuple = 1,2,3,4
-print(my_tuple)
+# 当元组不是空元组时，括号可以省略
+# 如果元组不是空元组，它里边至少要有一个,
+my_tuple = 10,20,30,40
+my_tuple = 40,
+# print(my_tuple , type(my_tuple))
+
+my_tuple = 10 , 20 , 30 , 40
 
 # 元组的解包（解构）
-# 把每一个元素赋值给一个变量
+# 解包指就是将元组当中每一个元素都赋值给一个变量
 a,b,c,d = my_tuple
-# print('a =',a)
-# print('b =',b)
 
+# print("a =",a)
+# print("b =",b)
+# print("c =",c)
+# print("d =",d)
 
 a = 100
-b = 200
+b = 300
+# print(a , b)
 
-print(a,b)
-# 交换a和b的值，可以使用元组的解包
+# 交互a 和 b的值，这时我们就可以利用元组的解包
 a , b = b , a
-print(a , b)
 
-# 对一个元组进行解包时，变量的数量必须对元组中的元素数量一致
-# 也可以再变量前添加一个*，这样变量将会获取元组中所有的剩余数量
-# 不能出现两个或者以上的*，否则会报错
-a, b, *c = my_tuple
-print(a,b)
-print('c = ',c)
+# print(a , b)
+my_tuple = 10 , 20 , 30 , 40
+
+
+# 在对一个元组进行解包时，变量的数量必须和元组中的元素的数量一致
+# 也可以在变量前边添加一个*，这样变量将会获取元组中所有剩余的元素
+a , b , *c = my_tuple
+a , *b , c = my_tuple
+*a , b , c = my_tuple
+a , b , *c = [1,2,3,4,5,6,7]
+a , b , *c = 'hello world'
+# 不能同时出现两个或以上的*变量
+# *a , *b , c = my_tuple SyntaxError: two starred expressions in assignment
+print('a =',a)
+print('b =',b)
+print('c =',c)
+ 
